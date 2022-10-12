6a8cb2a69466852695eb74b165173e2fdfbe9908
JoshSocha <joshua.socha@gmail.com>
2022-10-12 07:16:23 -0500

    Added Summer Raffle winners.

--- application/views/pages/EventsAndNews/SummerRaffle.php
+++ application/views/pages/EventsAndNews/SummerRaffle.php
@@ -6,29 +6,29 @@ Where: De Pere Sportsmen's Club</p>
 <p>Please assist us in this annual Club activity; buy a ticket, sell a ticket. It all helps to complete the physical improvement work of the Club.</p>
 <table align="center" class="table-with-header">
 	<thead>
-		<tr><th></th><th>Prize</th></tr>
+		<tr><th></th><th>Prize</th><th>Winner</th></tr>
 	</thead>
 	<tbody>
-		<tr><td>1</td><td>$700</td></tr>
-		<tr><td>2</td><td>Christensen Arms Mesa, Bolt Action 300 Win Mag</td></tr>
-		<tr><td>3</td><td>$200</td></tr>
-		<tr><td>4</td><td>Taurus G3C, 9 mm Pistol</td></tr>
-		<tr><td>5</td><td>$300</td></tr>
-		<tr><td>6</td><td>$150</td></tr>
-		<tr><td>7</td><td>Traditions SS Muzzleloader with Scope, .50 cal</td></tr>
-		<tr><td>8</td><td>$575</td></tr>
-		<tr><td>9</td><td>Weatherby Orion Sporting O/U, 12 ga</td></tr>
-		<tr><td>10</td><td>$425</td></tr>
-		<tr><td>11</td><td>Heritage Rough Rider Revolver, .22 LR</td></tr>
-		<tr><td>12</td><td>$500</td></tr>
-		<tr><td>13</td><td>JTS Tactical Pump Shotgun, 12 ga</td></tr>
-		<tr><td>14</td><td>$625</td></tr>
-		<tr><td>15</td><td>Savage SS Bolt Action Rifle, Bull Barrel, 17 HMR</td></tr>
-		<tr><td>16</td><td>$250</td></tr>
-		<tr><td>17</td><td>$775</td></tr>
-		<tr><td>18</td><td>Bergara B14 HMR, Mini Chassis Rifle, .22-250</td></tr>
-		<tr><td>19</td><td>$450</td></tr>
-		<tr><td>20</td><td>Glock 43X, Apollo Custom (FDE), 9 mm Pistol</td></tr>
+		<tr><td>1</td><td>$700</td><td>Kevin Myer</td></tr>
+		<tr><td>2</td><td>Christensen Arms Mesa, Bolt Action 300 Win Mag</td><td>Al Weiner</td></tr>
+		<tr><td>3</td><td>$200</td><td>Phil Prokash</td></tr>
+		<tr><td>4</td><td>Taurus G3C, 9 mm Pistol</td><td>Michael Zittlow</td></tr>
+		<tr><td>5</td><td>$300</td><td>Jason Wellner</td></tr>
+		<tr><td>6</td><td>$150</td><td>Randy and Cheryl Scray</td></tr>
+		<tr><td>7</td><td>Traditions SS Muzzleloader with Scope, .50 cal</td><td>Wendi Lemmen</td></tr>
+		<tr><td>8</td><td>$575</td><td>Gregg Marweg</td></tr>
+		<tr><td>9</td><td>Weatherby Orion Sporting O/U, 12 ga</td><td>Jim Sausen</td></tr>
+		<tr><td>10</td><td>$425</td><td>Dorothy Cashman</td></tr>
+		<tr><td>11</td><td>Heritage Rough Rider Revolver, .22 LR</td><td>Roland Calkins</td></tr>
+		<tr><td>12</td><td>$500</td><td>Charlie Kaftan</td></tr>
+		<tr><td>13</td><td>JTS Tactical Pump Shotgun, 12 ga</td><td>Samantha DuBois</td></tr>
+		<tr><td>14</td><td>$625</td><td>Vayda Landwher</td></tr>
+		<tr><td>15</td><td>Savage SS Bolt Action Rifle, Bull Barrel, 17 HMR</td><td>Sue Pfeiffer</td></tr>
+		<tr><td>16</td><td>$250</td><td>Susan Gilme</td></tr>
+		<tr><td>17</td><td>$775</td><td>Darren Vollmer</td></tr>
+		<tr><td>18</td><td>Bergara B14 HMR, Mini Chassis Rifle, .22-250</td><td>Chad Black</td></tr>
+		<tr><td>19</td><td>$450</td><td>Bob VanderZanden</td></tr>
+		<tr><td>20</td><td>Glock 43X, Apollo Custom (FDE), 9 mm Pistol</td><td>Scott Sievert</td></tr>
 	</tbody>
 </table>
 <p>Guns are purchased through JEL Enterpirse (Josh Landwher)</p>
--- application/views/pages/Home.php
+++ application/views/pages/Home.php
@@ -9,6 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <h2>Sight-In Days</h2>
 <p>October 1 and 2 are our first sight-in days for 2022.  Check out the <a href="<?php echo base_url();?>EventsAndNews/SightInDays">sight-in days</a> page for more details.</p>
 <hr></hr>
+<h2>$ummer Raffle</h2>
+<p>Check out the winners at our <a href="<?php echo base_url();?>EventsAndNews/SummerRaffle">$ummer Raffle</a> page.</p>
+<hr></hr>
 <h1>Who we are...</h1>
 <p>Welcome to the DePere Sportsmen’s Club! Established in 1952 by a group of dedicated volunteer sportsmen to further promote shooting sports in the Green Bay area, the De Pere Sportsmen's Club continues to provide opportunities for people of all ages to experience the benefits and beauty of a lovely natural surrounding while continuing to educate community members, and promote a wide range of shooting sports in a safe appropriate facility. The legacy of the founders continues today through the regular efforts of volunteers. Check in with a Board member to find out how you can help continue the legacy for future generations.</p>
 <p>De Pere Sportsmen's Club offers a wide variety of leagues and annual events for you and your family, whether you are an experienced shooter, or a newbie looking for a place to learn and experience shooting sports. Do you have a son or daughter who would like to learn how to shoot? There are always experienced shooters both available and willing to help you with the basics, and steer you or your child into another Club opportunity. Emphasis is always placed on safety first to establish safe gun and bow handling habits. The De Pere Sportsmen's Club really takes an active interest in getting shooters started off right, and including them in the activities of the Club. De Pere Sportsmen's Club is proud of their collective role in bringing these fun sports to new generations of enthusiasts.</p>
